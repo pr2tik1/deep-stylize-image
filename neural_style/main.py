2ef30a7b2fbb9e3c73dacbacdeec7b4414ee67ab
pr2tik1 <pr2tik1@gmail.com>
2021-03-06 23:53:25 +0530

    added files

---
+++ neural_style/main.py
@@ -0,0 +1,38 @@
+import streamlit as st 
+from PIL import Image 
+import style 
+
+im = Image.open('banner.jpg')
+st.image(im)
+
+st.write("Stylize your Image from the available styles. Just choose from the options in the sidebar and click '**stylize**'. \
+    This app uses style transfer deep learning technique. To know more follow the model training and code visit [here](https://github.com/pytorch/examples/tree/master/fast_neural_style).")
+
+img = st.sidebar.selectbox(
+    "Select Image",
+    ("amber.jpg","dog.jpg","author.png","cat.jpg","flower.jpg")
+)
+
+style_name = st.sidebar.selectbox(
+    "Select Style",
+    ('candy','mosaic','udnie','rain_princess')
+)
+
+model = "saved_models/" + style_name + ".pth"
+input_image = "images/content-images/" + img 
+output_image = "images/output-images/" + style_name + "-" + img 
+
+st.write("### Source Image: ")
+image = Image.open(input_image)
+st.image(image, width=500)
+
+clicked = st.button("Stylize")
+if clicked:
+    model = style.load_model(model)
+    style.stylize(model, input_image, output_image)
+
+    st.write("### Output Image")
+    image = Image.open(output_image)
+    st.image(image, width=500)
+
+st.write("To save: Right click -> Save Image as")
