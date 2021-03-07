0ad6452dcfa954b4ce99996ec6883599e75bf4c5
pr2tik1 <pr2tik1@gmail.com>
2021-03-07 10:00:49 +0530

    added more func

--- neural_style/app.py
+++ neural_style/app.py
@@ -6,7 +6,9 @@ im = Image.open('banner.jpg')
 st.image(im)
 
 st.write("Stylize your Image from the available styles. Just choose from the options in the sidebar and click '**stylize**'. \
-    This app uses style transfer deep learning technique. To know more follow the model training and code visit [here](https://github.com/pytorch/examples/tree/master/fast_neural_style).")
+    This app uses style transfer deep learning technique. To know more follow the model training and code visit \
+        [here](https://github.com/pytorch/examples/tree/master/fast_neural_style). **Note** first run image and style from \
+            sidebar, then upload your own image.")
 
 img = st.sidebar.selectbox(
     "Select Image",
@@ -19,12 +21,20 @@ style_name = st.sidebar.selectbox(
 )
 
 model = "saved_models/" + style_name + ".pth"
-input_image = "images/content-images/" + img 
+input_image = "images/content-images/" + img  
 output_image = "images/output-images/" + style_name + "-" + img 
 
+image_uploaded = st.file_uploader("Upload an image: ", type="jpg")
+
+if image_uploaded is not None:
+    input_ = image_uploaded
+    image = Image.open(image_uploaded)
+else:
+    input_ = input_image
+    image = Image.open(input_image)
+
 st.write("### Source Image: ")
-image = Image.open(input_image)
-st.image(image, width=500)
+st.image(image, use_column_width='always')
 
 clicked = st.button("Stylize")
 if clicked:
@@ -33,6 +43,6 @@ if clicked:
 
     st.write("### Output Image")
     image = Image.open(output_image)
-    st.image(image, width=500)
+    st.image(image, use_column_width='always')
 
-st.write("To save: Right click -> Save Image as")
+st.write("What a style!")
