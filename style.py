1f04ab133e96477dbd0cd2628271683d6e87c648
pr2tik1 <pr2tik1@gmail.com>
2021-03-11 00:31:52 +0530

    try to compress1

--- style.py
+++ style.py
@@ -3,19 +3,11 @@ import os
 import sys
 import time
 import re
-
-import numpy as np
 import torch
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision import transforms
-import torch.onnx
 
 import utils
 from transformer_net import TransformerNet
-from vgg import Vgg16
-
+from torchvision import transforms
 import streamlit as st
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
